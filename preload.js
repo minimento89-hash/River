39829b27c9417396a8b259c0bd0e68ccf3c882b9
minimento89-hash <minimento89@gmail.com>
2025-10-22 19:37:33 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose a limited set of APIs to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Example: You could expose a function to save files here
-  // saveFile: (data) => ipcRenderer.invoke('save-file', data)
-});
