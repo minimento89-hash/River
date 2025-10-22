a44ef4a5f47895679737e56886e1bb4a046a08a8
minimento89-hash <minimento89@gmail.com>
2025-10-22 19:36:49 +0200

    Delete jest.config.ts

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-};
